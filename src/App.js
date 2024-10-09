a0746e69bbd95e660ed81f5da085589a5e631a29
andresLamsu <perezmartinezandres0@gmail.com>
2024-10-09 17:23:28 -0500

    Se añadio ventana de login ya configurada con el back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {RegisterPage} from "./Pages/RegisterPage";
+import {LogInPage} from "./Pages/LogInPage";
 
 function App() {
   return (
       <div>
-        <RegisterPage/>
+        <LogInPage/>
       </div>
   );
 }
---
+++ src/Pages/LogInPage.jsx
@@ -0,0 +1,14 @@
+import {BgLog} from "../components/BackGround_Component/BgLog";
+import {LoginComponent} from "../components/Login_Component/LoginComponent";
+
+function LogInPage() {
+    return (
+
+        <BgLog
+            COMPONENT={LoginComponent}
+        />
+
+    );
+}
+
+export {LogInPage};
